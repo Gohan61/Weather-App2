625d71bc4f8db0bcbda85b3955c1518be4ce23c3
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-01 10:20:54 +0100

    Substract weather data from API in object

--- play.js
+++ play.js
@@ -1,22 +1,60 @@
 const input = document.querySelector("#location");
 const searchButton = document.querySelector(".searchButton");
+const weatherToday = document.querySelector(".weatherToday");
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-  getWeather();
+  weatherDataObj().then((obj) => display(obj));
 });
 
-async function getWeather() {
+const weatherDataObj = async function getWeather() {
   try {
-    fetch(
-      `https://api.weatherapi.com/v1/current.json?key=11111111111111111&q=${input.value}`,
+    const respons = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=f5793d9a1dc240899c454904231007&q=${input.value}`,
       { mode: "cors" }
-    )
-      .then((respons) => respons.json())
-      .then((response) => {
-        console.log(response);
-      });
+    );
+    const weatherData = await respons.json();
+    console.log(await weatherData);
+
+    const getWeatherData = async () => {
+      const weatherForLocation = {
+        cloud: {
+          element: document.createElement("div"),
+          data: await weatherData.current.cloud,
+          text: "Cloud: ",
+        },
+        feelsLike: {
+          element: document.createElement("div"),
+          data: await weatherData.current.feelslike_c,
+          text: "Feels like: ",
+        },
+        humidity: {
+          element: document.createElement("div"),
+          data: await weatherData.current.humidity,
+          text: "Humidity: ",
+        },
+        temperature: {
+          element: document.createElement("div"),
+          data: await weatherData.current.temp_c,
+          text: "Temperature: ",
+        },
+        wind: {
+          element: document.createElement("div"),
+          data: await weatherData.current.wind_kph,
+          text: "Wind: ",
+        },
+      };
+      return weatherForLocation;
+    };
+    return { getWeatherData };
   } catch (error) {
     console.log(error);
   }
+};
+
+function display(obj) {
+  obj.getWeatherData().then((weather) => {
+    weather.cloud.element.textContent = weather.cloud.text + weather.cloud.data;
+    weatherToday.append(weather.cloud.element);
+  });
 }
