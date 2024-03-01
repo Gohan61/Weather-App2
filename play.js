ba42586492d8042f132b52860e9fcb0c527bffb2
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-01 14:19:12 +0100

    Call + display weater tomorrow + day after

--- play.js
+++ play.js
@@ -1,6 +1,10 @@
 const input = document.querySelector("#location");
 const searchButton = document.querySelector(".searchButton");
 const weatherToday = document.querySelector(".weatherToday");
+const weatherTomorrow = document.querySelector(".weatherTomorrow");
+const weatherDayAfterTomorrow = document.querySelector(
+  ".weatherDayAfterTomorrow"
+);
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -10,23 +14,23 @@ searchButton.addEventListener("click", (e) => {
 const weatherDataObj = async function getWeather() {
   try {
     const respons = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=f5793d9a1dc240899c454904231007&q=${input.value}`,
+      `http://api.weatherapi.com/v1/forecast.json?key=f5793d9a1dc240899c454904231007&q=${input.value}&days=3&aqi=no&alerts=no}`,
       { mode: "cors" }
     );
     const weatherData = await respons.json();
     console.log(await weatherData);
 
     const getWeatherData = async () => {
-      const weatherForLocation = {
+      const weatherForLocationToday = {
         cloud: {
           element: document.createElement("div"),
-          data: await weatherData.current.cloud,
-          text: "Cloud: ",
+          data: await weatherData.current.condition.text,
+          text: "Description: ",
         },
-        feelsLike: {
+        chanceOfRain: {
           element: document.createElement("div"),
           data: await weatherData.current.feelslike_c,
-          text: "Feels like: ",
+          text: "Chance of rain (%): ",
         },
         humidity: {
           element: document.createElement("div"),
@@ -41,10 +45,70 @@ const weatherDataObj = async function getWeather() {
         wind: {
           element: document.createElement("div"),
           data: await weatherData.current.wind_kph,
-          text: "Wind: ",
+          text: "Wind (km/h): ",
         },
       };
-      return weatherForLocation;
+      const weatherForLocationTomorrow = {
+        cloud: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["1"].day.condition.text,
+          text: "Description: ",
+        },
+        chanceOfRain: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["1"].day
+            .daily_chance_of_rain,
+          text: "Chance of rain (%): ",
+        },
+        humidity: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["1"].day.avghumidity,
+          text: "Humidity: ",
+        },
+        temperature: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["1"].day.avgtemp_c,
+          text: "Temperature: ",
+        },
+        wind: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["1"].day.maxwind_kph,
+          text: "Wind (max. km/h): ",
+        },
+      };
+      const weatherForLocationDayAfterTomorrow = {
+        cloud: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["2"].day.condition.text,
+          text: "Description: ",
+        },
+        chanceOfRain: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["2"].day
+            .daily_chance_of_rain,
+          text: "Chance of rain (%): ",
+        },
+        humidity: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["2"].day.avghumidity,
+          text: "Humidity: ",
+        },
+        temperature: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["2"].day.avgtemp_c,
+          text: "Temperature: ",
+        },
+        wind: {
+          element: document.createElement("div"),
+          data: await weatherData.forecast.forecastday["2"].day.maxwind_kph,
+          text: "Wind (max. km/h): ",
+        },
+      };
+      return {
+        weatherForLocationToday,
+        weatherForLocationTomorrow,
+        weatherForLocationDayAfterTomorrow,
+      };
     };
     return { getWeatherData };
   } catch (error) {
@@ -54,7 +118,60 @@ const weatherDataObj = async function getWeather() {
 
 function display(obj) {
   obj.getWeatherData().then((weather) => {
-    weather.cloud.element.textContent = weather.cloud.text + weather.cloud.data;
-    weatherToday.append(weather.cloud.element);
+    addAttributes(weather.weatherForLocationToday);
+    addAttributes(weather.weatherForLocationTomorrow);
+    addAttributes(weather.weatherForLocationDayAfterTomorrow);
+    appendObjects(
+      [
+        weather.weatherForLocationToday.cloud.element,
+        weather.weatherForLocationToday.chanceOfRain.element,
+        weather.weatherForLocationToday.humidity.element,
+        weather.weatherForLocationToday.temperature.element,
+        weather.weatherForLocationToday.wind.element,
+      ],
+      weatherToday
+    );
+    appendObjects(
+      [
+        weather.weatherForLocationTomorrow.cloud.element,
+        weather.weatherForLocationTomorrow.chanceOfRain.element,
+        weather.weatherForLocationTomorrow.humidity.element,
+        weather.weatherForLocationTomorrow.temperature.element,
+        weather.weatherForLocationTomorrow.wind.element,
+      ],
+      weatherTomorrow
+    );
+    appendObjects(
+      [
+        weather.weatherForLocationDayAfterTomorrow.cloud.element,
+        weather.weatherForLocationDayAfterTomorrow.chanceOfRain.element,
+        weather.weatherForLocationDayAfterTomorrow.humidity.element,
+        weather.weatherForLocationDayAfterTomorrow.temperature.element,
+        weather.weatherForLocationDayAfterTomorrow.wind.element,
+      ],
+      weatherDayAfterTomorrow
+    );
   });
+}
+
+function appendObjects(object, appendTo) {
+  object.forEach((el) => {
+    appendTo.append(el);
+  });
+}
+
+function addAttributes(object) {
+  for (const item in object) {
+    if (object[item] === object["element"]) {
+      continue;
+    }
+    for (const [key, value] of Object.entries(object[item])) {
+      if (key === "element") {
+        continue;
+      } else {
+        object[item]["element"].textContent =
+          object[item]["text"] + object[item]["data"];
+      }
+    }
+  }
 }
